2dc5ba6574edb72bd55133dafc17e366f42f4ae9
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-09 08:59:07 +0000

    Refactor complete-tree check by introducing helper function to meet Betty's 40-line rule

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 
 /**
+ * check_complete - Helper that verifies if a tree is complete
+ * @tree: Pointer to the root node
+ * @queue: Array used as a queue for level-order traversal
+ *
+ * Return: 1 if tree is complete, 0 otherwise
+ */
+static int check_complete(const binary_tree_t *tree,
+		const binary_tree_t **queue)
+{
+	int front = 0, rear = 0, flag = 0;
+	const binary_tree_t *node;
+
+	queue[rear++] = tree;
+
+	while (front < rear)
+	{
+		node = queue[front++];
+
+		if (node->left)
+		{
+			if (flag)
+				return (0);
+			queue[rear++] = node->left;
+		}
+		else
+			flag = 1;
+
+		if (node->right)
+		{
+			if (flag)
+				return (0);
+			queue[rear++] = node->right;
+		}
+		else
+			flag = 1;
+	}
+
+	return (1);
+}
+
+/**
  * binary_tree_is_complete - Checks if a binary tree is complete
  * @tree: Pointer to the root node of the tree
  *
@@ -10,9 +51,8 @@
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	const binary_tree_t **queue;
-	int front = 0, rear = 0;
-	int flag = 0;
 	int capacity = 1024;
+	int result;
 
 	if (!tree)
 		return (0);
@@ -21,40 +61,9 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (!queue)
 		return (0);
 
-	queue[rear++] = tree;
-
-	while (front < rear)
-	{
-		const binary_tree_t *node = queue[front++];
-
-		/* Left child */
-		if (node->left)
-		{
-			if (flag)
-			{
-				free(queue);
-				return (0);
-			}
-			queue[rear++] = node->left;
-		}
-		else
-			flag = 1;
-
-		/* Right child */
-		if (node->right)
-		{
-			if (flag)
-			{
-				free(queue);
-				return (0);
-			}
-			queue[rear++] = node->right;
-		}
-		else
-			flag = 1;
-	}
+	result = check_complete(tree, queue);
 
 	free(queue);
-	return (1);
+	return (result);
 }
 

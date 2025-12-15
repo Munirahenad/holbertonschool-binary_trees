ce7c2ec6cb1fb87870f77461ff0d6c45770a5666
munirahenad <muneraenad@hotmail.com>
2025-12-15 09:22:20 +0000

    Simple and efficient O(1) time complexity

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_node - Creates a binary tree node
  * @parent: Pointer to the parent node
- * @value: Value to store in the new node
+ * @value: Value to put in the new node
  *
  * Return: Pointer to the new node, or NULL on failure
  */
@@ -23,4 +22,3 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	return (new_node);
 }
-
